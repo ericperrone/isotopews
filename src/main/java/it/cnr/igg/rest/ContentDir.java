03b30a0300c6c0de8cbd34a1836bd7f0ca5d5c7e
ericperrone <87177481+ericperrone@users.noreply.github.com>
2023-07-31 14:29:37 +0200

    Update ContentDir.java

--- src/main/java/it/cnr/igg/rest/ContentDir.java
+++ src/main/java/it/cnr/igg/rest/ContentDir.java
@@ -14,6 +14,7 @@ import com.google.gson.Gson;
 import java.util.ArrayList;
 import it.cnr.igg.helper.Global;
 import it.cnr.igg.helper.ResultBuilder;
+import it.cnr.igg.sheetx.csv.Csv;
 import it.cnr.igg.sheetx.xlsx.Xlsx;
 import it.cnr.igg.sheetx.xlsx.Xsl;
 import it.cnr.igg.sheetx.xlsx.Sheetx;
@@ -39,16 +40,15 @@ public class ContentDir extends ResultBuilder {
 			return error(x.getMessage());
 		}
 	}
-	
+
 	@Path("/process-file")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response processFile(@QueryParam("fileName") String name) {
 		String key = null;
 		ArrayList<String> sheets = null;
-		try {			
+		try {
 			if (name.toLowerCase().endsWith(".xlsx")) {
-				// Global.xls = new Xls(Global.dataFolder + Global.fileSeparator + name);
 				key = Global.openXlsx(name);
 				Xlsx xls = Global.getXlsx(key);
 				sheets = xls.getSheets();
@@ -67,12 +67,29 @@ public class ContentDir extends ResultBuilder {
 			Gson gson = new Gson();
 			json = gson.toJson(ch);
 			return ok(json);
-			
+
 		} catch (Exception x) {
 			return error(x.getMessage());
 		}
 	}
-	
+
+	@Path("/get-content-csv")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getContent(@QueryParam("fileName") String fileName) {
+		ArrayList<ArrayList<String>> content = null;
+		try {
+			Csv csv = new Csv(Global.dataFolder + Global.fileSeparator + fileName);
+			content = csv.getContent();
+			String json = "";
+			Gson gson = new Gson();
+			json = gson.toJson(content);
+			return ok(json);
+		} catch (Exception x) {
+			return error(x.getMessage());
+		}
+	}
+
 	@Path("/get-content-xls")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
@@ -82,9 +99,9 @@ public class ContentDir extends ResultBuilder {
 				ArrayList<ArrayList<String>> content = null;
 				Sheetx sheetx = Global.getSheet(key);
 				if (sheetx instanceof Xlsx)
-					content = ((Xlsx)sheetx).getContent(sheet);
+					content = ((Xlsx) sheetx).getContent(sheet);
 				else
-					content = ((Xsl)sheetx).getContent(sheet);
+					content = ((Xsl) sheetx).getContent(sheet);
 				String json = "";
 				Gson gson = new Gson();
 				json = gson.toJson(content);
@@ -96,20 +113,21 @@ public class ContentDir extends ResultBuilder {
 			return error(x.getMessage());
 		}
 	}
-	
+
 	@Path("/close-contentdir")
 	@OPTIONS
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response closeContentDir() {
 		return ok();
 	}
-	
+
 	@Path("/close-contentdir")
 	@DELETE
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response closeContentDir(@QueryParam("key") String key) {
 		try {
-			Global.releasXls(key);
+			if (key != null && key.length() > 0)
+				Global.releasXls(key);
 			return ok();
 		} catch (Exception x) {
 			return error(x.getMessage());
