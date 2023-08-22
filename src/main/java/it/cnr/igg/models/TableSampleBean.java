1af0f3b502211738d83cb00d22ebb1b98336c411
ericperrone <87177481+ericperrone@users.noreply.github.com>
2023-08-22 14:46:15 +0200

    main query

---
+++ src/main/java/it/cnr/igg/models/TableSampleBean.java
@@ -0,0 +1,122 @@
+package it.cnr.igg.models;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import it.cnr.igg.isotopedb.beans.SampleBean;
+import it.cnr.igg.isotopedb.beans.SampleFieldBean;
+import it.cnr.igg.isotopedb.beans.ComponentBean;
+
+class TableItem {
+	public String headerName;
+	public int headerColumn;
+	public String value;
+}
+
+public class TableSampleBean {
+	private ArrayList<String> header;
+	private ArrayList<ArrayList<String>> body;
+	private HashMap<String, TableItem> index;
+
+	public TableSampleBean() {
+		header = new ArrayList<String>();
+		body = new ArrayList<ArrayList<String>>();
+		index = new HashMap<String, TableItem>();
+	}
+
+	public ArrayList<String> getHeader() {
+		return header;
+	}
+
+	public ArrayList<ArrayList<String>> getBody() {
+		return body;
+	}
+
+	public HashMap<String, TableItem> getIndex() {
+		return index;
+	}
+
+	public void build(List<SampleBean> beans) {
+		buildHeader(beans);
+		body.add(header);
+		buildBody(beans);
+	}
+
+	private void buildBody(List<SampleBean> beans) {
+		int rowElements = header.size();
+		for (SampleBean s : beans) {
+			ArrayList<String> row = new ArrayList<String>();
+			for (int i = 0; i < rowElements; i++) {
+				row.add("");
+			}
+			List<SampleFieldBean> fields = s.getFields();
+			if (fields != null) {
+				for (SampleFieldBean f : fields) {
+					TableItem item = index.get(f.getFieldName());
+					row.set(item.headerColumn, f.getFieldValue());
+				}
+			}
+			List<ComponentBean> components = s.getComponents();
+			if (components != null) {
+				for (ComponentBean c : components) {
+					TableItem item = index.get(c.getComponent());
+					row.set(item.headerColumn, "" + c.getValue());
+				}
+			}
+			body.add(row);
+		}
+	}
+
+	private void buildHeader(List<SampleBean> beans) {
+		int position = 0;
+		for (SampleBean s : beans) {
+			List<SampleFieldBean> fields = s.getFields();
+			if (fields != null) {
+				for (SampleFieldBean f : fields) {
+					if (index.get(f.getFieldName()) == null) {
+						TableItem item = new TableItem();
+						item.headerColumn = position;
+						item.headerName = f.getFieldName();
+						index.put(f.getFieldName(), item);
+						position++;
+						header.add(f.getFieldName());
+					}
+				}
+			}
+		}
+		for (SampleBean s : beans) {
+			List<ComponentBean> components = s.getComponents();
+			if (components != null) {
+				for (ComponentBean f : components) {
+					if (index.get(f.getComponent()) == null) {
+						if (!f.getIsIsotope()) {
+							TableItem item = new TableItem();
+							item.headerColumn = position;
+							item.headerName = f.getComponent();
+							index.put(f.getComponent(), item);
+							position++;
+							header.add(f.getComponent());
+						}
+					}
+				}
+			}
+		}
+		for (SampleBean s : beans) {
+			List<ComponentBean> components = s.getComponents();
+			if (components != null) {
+				for (ComponentBean f : components) {
+					if (index.get(f.getComponent()) == null) {
+						if (f.getIsIsotope()) {
+							TableItem item = new TableItem();
+							item.headerColumn = position;
+							item.headerName = f.getComponent();
+							index.put(f.getComponent(), item);
+							position++;
+							header.add(f.getComponent());
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ src/main/java/it/cnr/igg/rest/Query.java
@@ -0,0 +1,115 @@
+package it.cnr.igg.rest;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.OPTIONS;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.google.gson.Gson;
+import com.google.gson.internal.LinkedTreeMap;
+
+import it.cnr.igg.helper.RestResult;
+import it.cnr.igg.helper.ResultBuilder;
+import it.cnr.igg.isotopedb.beans.ComponentBean;
+import it.cnr.igg.isotopedb.beans.SampleBean;
+import it.cnr.igg.isotopedb.beans.SampleFieldBean;
+import it.cnr.igg.isotopedb.exceptions.DbException;
+import it.cnr.igg.isotopedb.queries.MainQuery;
+import it.cnr.igg.isotopedb.tools.QueryFilter;
+import it.cnr.igg.models.TableSampleBean;
+
+@Path("")
+public class Query extends ResultBuilder {
+	@Context
+	private HttpServletRequest request;
+
+	@Path("/get-samples")
+	@OPTIONS
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getSamplesOpt() {
+		return ok("");
+	}
+	
+	@Path("/get-samples")
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getSamples(@QueryParam("meta") String meta, 
+			@QueryParam("auth") String authors,
+			@QueryParam("ref") String ref,
+			@QueryParam("year") String year) {
+		try {
+			ArrayList<SampleBean> beans = querySamples(meta, authors, ref, year);
+			TableSampleBean tsb = new TableSampleBean();
+			tsb.build(beans);
+			String json = "";
+			Gson gson = new Gson();
+			json = gson.toJson(tsb.getBody());
+			return ok(json);
+		} catch (Exception ex) {
+			return error(ex.getMessage());
+		}
+	}
+	
+	
+	@Path("/query")
+	@OPTIONS
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response queryOpt() {
+		return ok("");
+	}
+
+	@Path("/query")
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response query(@QueryParam("meta") String meta, 
+			@QueryParam("auth") String authors,
+			@QueryParam("ref") String ref,
+			@QueryParam("year") String year) {
+		try {
+			ArrayList<SampleBean> beans = querySamples(meta, authors, ref, year);
+			String json = "";
+			Gson gson = new Gson();
+			json = gson.toJson(beans);
+			return ok(json);
+		} catch (Exception ex) {
+			return error(ex.getMessage());
+		}
+	}
+	
+	private ArrayList<SampleBean> querySamples(String meta,
+			String authors,
+			String ref,
+			String year) throws Exception, DbException {
+		MainQuery mainQuery = new MainQuery();
+		QueryFilter filter = new QueryFilter();
+		if (meta != null) {
+			filter.keywords = meta.split(" ");
+		}
+		if (authors != null) {
+			filter.authors = authors.split(",");
+		}
+		if (ref != null) {
+			filter.ref = ref;
+		}
+		if (year != null) {
+			filter.year = Integer.valueOf(year);
+		}
+		ArrayList<SampleBean> beans = mainQuery.query(filter);
+		return beans;
+	}
+
+}
