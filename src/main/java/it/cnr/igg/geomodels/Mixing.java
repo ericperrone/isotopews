239b37573765166df5612a994bbcdd23f30e43c3
ericperrone <87177481+ericperrone@users.noreply.github.com>
2023-09-28 11:30:06 +0200

    using BigDecimal instead of Double

--- src/main/java/it/cnr/igg/geomodels/Mixing.java
+++ src/main/java/it/cnr/igg/geomodels/Mixing.java
@@ -1,6 +1,7 @@
 package it.cnr.igg.geomodels;
 
 import java.util.ArrayList;
+import java.math.BigDecimal;
 
 class Mix {
 	public double weight;
@@ -39,16 +40,16 @@ public class Mixing {
 		this.step = step;
 	}
 	
-	public ArrayList<MixingResult> getResult() {
-		return this.result;
-	}
-	
 	public void setStartValue(double startValue) {
 		this.startValue = startValue;
 	}
 
 	public void setEndValue(double endValue) {
 		this.endValue = endValue;
+	}
+
+	public ArrayList<MixingResult> getResult() {
+		return this.result;
 	}
 	
 	public void apply() {
@@ -62,30 +63,55 @@ public class Mixing {
 					mr.element = gd.getElement();
 					mr.memberA = m[0].member;
 					mr.memberB = m[1].member;
-					for (double w = startValue; w <= endValue; w+= step) {
-						double value = compute(m[0].value, m[1].value, w);
-						System.out.println("" + value);
-						mr.mix.add(new Mix(w, value));
+					BigDecimal w = BigDecimal.valueOf(startValue);
+					BigDecimal end = BigDecimal.valueOf(endValue);
+					BigDecimal increment = BigDecimal.valueOf(step);
+					while (w.compareTo(end) <= 0) {
+						BigDecimal bValue = compute(BigDecimal.valueOf(m[0].value), 
+								BigDecimal.valueOf(m[1].value), 
+								w);
+						mr.mix.add(new Mix(w.doubleValue(), bValue.doubleValue()));
+						System.out.println(w + ": " + bValue);
+						w = w.add(increment);
 					}
+					
 					result.add(mr);
 				}
 			}
 		}
 	}
 
-	public double compute(double endMember1, double endMember2, double weightProportion) {
-		return weightProportion * (endMember1 - endMember2) + endMember2;
+	public BigDecimal compute(BigDecimal endMember1, BigDecimal endMember2, BigDecimal weightProportion) {
+		BigDecimal result = endMember1;
+		result = result.subtract(endMember2);
+		result = result.multiply(weightProportion);
+		result = result.add(endMember2);
+		return result;
+		// return weightProportion * (endMember1 - endMember2) + endMember2;
 	}
 	
 	public static void main(String[] args) {
-		GeoData geo = new GeoData();
-		geo.setElement("Ba");
-		Member[] members = { new Member("EA136", 554d), new Member("EA185", 1143d) };
-		geo.setMembers(members);
-		GeoData[] geodata = { geo };
-		Mixing mixing = new Mixing(geodata);
-		mixing.apply();
-		ArrayList<MixingResult> result = mixing.getResult();
+		double a = 0.05d, b = 0.01d;		
+		System.out.println("a: " + a + ", b: " + b + ", a+b: " + (a+b));
+		String aa = "0.05", bb = "0.01";
+//		BigDecimal bda = new BigDecimal("" + a);
+//		BigDecimal bdb = new BigDecimal("" + b);
+		BigDecimal bda = BigDecimal.valueOf(a);
+		BigDecimal bdb = BigDecimal.valueOf(b);
+		BigDecimal bsum = bda.add(bdb);
+		System.out.println("a: " + bda + ", b: " + bdb + ", a+b: " + bsum);
+		
+		
+//		System.out.println("a: " + a + ", b: " + b + ", nextDown(a+b): " + Math.nextDown(a+b));
+//		System.out.println("a: " + a + ", b: " + b + ", a+b: " + Math.round(a+b));
+//		GeoData geo = new GeoData();
+//		geo.setElement("Ba");
+//		Member[] members = { new Member("EA136", 554d), new Member("EA185", 1143d) };
+//		geo.setMembers(members);
+//		GeoData[] geodata = { geo };
+//		Mixing mixing = new Mixing(geodata);
+//		mixing.apply();
+//		ArrayList<MixingResult> result = mixing.getResult();
 	}
 
 }
